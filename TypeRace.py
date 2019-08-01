257b6b3ba7c47528694ac5996d81fc84e89d904d
Henning Lindig <henning.lindig@rogers.com>
2019-08-01 18:16:23 -0400

    occasionally inputs jargled text, very buggy

--- TypeRace.py
+++ TypeRace.py
@@ -5,18 +5,54 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
-
 from selenium.webdriver.common.by import By
 
+from bs4 import BeautifulSoup
 
-driver = webdriver.Chrome()
-driver.get("https://play.typeracer.com/")
+import time
 
-delay = 10 # seconds
-try:
-    myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'gwt-Anchor')))
-    print "Found"
-except TimeoutException:
-    print "Timed out"
+def startup():
+	driver = webdriver.Chrome()
+	driver.get("https://play.typeracer.com/")
+	driver.find_element_by_link_text('Enter a typing race').click()
+	inputTxt = getText(driver)
+	#print(getText(driver))
+	try:
+		countdown = WebDriverWait(driver, 5).until(
+			EC.presence_of_element_located((By.CLASS_NAME, "lightLabel")))
+	except TimeoutException:
+		print "failed to find lightLabel"
 
-driver.find_element_by_link_text('Enter a typing race').click()
+	try:
+		WebDriverWait(driver, 15).until(EC.staleness_of(countdown))
+		print "done"
+		sendText(driver, inputTxt)
+	except TimeoutException:
+		print "failed"
+
+def getText(driver):
+	html = driver.page_source
+	soup = BeautifulSoup(html, 'html.parser')
+	#for span in soup.find_all('span'):
+		#spanStr = span.encode("utf-8")
+	spanList = soup.find_all('span')
+	for i in range(len(spanList)):
+		spanStr = spanList[i].text
+		if spanStr.endswith('.'):
+			keysStr = spanList[i-2].text +  spanList[i-1].text + spanList[i].text
+			print("keysStr: " + keysStr)
+	return(keysStr)
+
+def sendText(driver, inputTxt):
+	print("send text")
+	actions = ActionChains(driver)
+	for c in inputTxt:
+		actions.send_keys(c)
+		time.sleep(1)
+		actions.perform()
+
+def main():
+	startup()
+
+if __name__ == "__main__":
+	main()
